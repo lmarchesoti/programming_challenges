c4daf8e3ef09b382f5ef58f7d9804a67c60ff184
lmarchesoti <lmarchesoti@hotmail.com>
2015-12-12 19:04:28 -0200

    brute force failed at archeologists dilemma

--- 110502/src/main.cpp
+++ 110502/src/main.cpp
@@ -5,14 +5,46 @@ using namespace std;
 /* data structures */
 
 /* global variables */
+unsigned int p, revp;
 
 /* function prototypes */
+// reverses p into revp
+unsigned int reverse(unsigned int);
+
+// checks if p and revp form a palindrome
+bool palindrome();
 
 /* debug */
 
 int main(){
 
+	int n, i;
+
+	cin >> n;
+
+	while(n--){
+
+		cin >> p; revp = reverse(p);
+
+		for(i = 0; !palindrome(); ++i) p = p + revp, revp = reverse(p);
+
+		cout << i << " " << p << endl;
+
+	}
+
   return 0;
 }
 
 /* functions */
+unsigned int reverse(unsigned int n){
+
+	unsigned int rev = 0;
+
+	// iteratively get the next digit from n and
+	// reformat rev to accomodate it
+	while(n != 0) rev *= 10, rev += n%10, n /= 10;
+
+	return rev;
+}
+
+bool palindrome(){ if (p == revp) return true; return false; }
--- 110503/src/main.cpp
+++ 110503/src/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <cmath>
 
 using namespace std;
 
@@ -7,12 +8,72 @@ using namespace std;
 /* global variables */
 
 /* function prototypes */
+// returns the number of digits of a number
+int size(unsigned long int);
+
+// reduces a number by a set amount of digits
+unsigned long int reduce(unsigned long int, int);
+
+// matches a number to the beginning of any power of 2
+int match(unsigned int);
 
 /* debug */
 
 int main(){
 
+	unsigned int n;
+	int result;
+
+	while(cin >> n){
+
+		if(n == 0) cout << "no power of 2" << endl;
+		else {
+
+			result = match(n);
+
+			if (result) cout << result << endl; else cout << "no power of 2" << endl; 
+		}
+	}
+
   return 0;
 }
 
 /* functions */
+int size(unsigned long int n){
+
+	int s = 0;
+
+	while(n != 0) ++s, n /= 10;
+
+	return s;
+
+}
+
+unsigned long int reduce(unsigned long int n, int s){
+
+	while(s) n /= 10, --s;
+
+	return n;
+
+}
+
+int match(unsigned int n){
+
+	unsigned long int t; // temporary power of 2
+	int s; // size of t
+
+	for(int e = 7; e <= 6000000; ++e){
+
+		t = pow(2, e);
+
+		s = size(t);
+
+		t = reduce(t, s/2 + 1);
+
+		while(n <= t) if (n == t) return e; else t = reduce(t, 1);
+
+	}
+
+	return 0;
+
+}
